ff8b40319c7e83b7e18d5eef6dca8ce1ee8b273d
Marthin Bergstrand <marthin.bergstrand@gmail.com>
2013-02-15 09:38:45 +0100

    Added the StringCalculator Dojo

---
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator
+{
+    class StringCalculator
+    {
+        private char[] ExtractDelimiters(string numbers)
+        {
+
+        }
+
+        public int add(String numbers) 
+        {
+            String[] digits = null;
+            int sum = 0;
+            char[] delimiters = new char[2];
+
+            if (numbers.StartsWith("//"))
+            {
+                delimiters[0] = numbers[2];
+                numbers = numbers.Substring(3);
+            }
+            else
+            {
+                delimiters[0] = ',';
+                delimiters[1] = '\n';
+            }
+
+            if (numbers.Equals(""))
+            {
+                return 0;
+            }
+
+            digits = numbers.Split(delimiters);
+
+            foreach (string digit in digits)
+            {
+                sum += int.Parse(digit);
+            }
+            return sum;
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator/TestSuite.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    class TestSuite
+    {
+        StringCalculator stringCalc;
+
+        [SetUp]
+        public void Before()
+        {
+            stringCalc = new StringCalculator();
+        }
+
+        [Test]
+        public void ReturnsZero()
+        {
+            Assert.AreEqual(0, stringCalc.add(""));
+        }
+
+        [Test]
+        public void ReturnsOneNUmber()
+        {
+            Assert.AreEqual(1, stringCalc.add("1"));
+        }
+
+        [Test]
+        public void ReturnsSumTwoNumbers()
+        {
+            Assert.AreEqual(3, stringCalc.add("1,2"));
+        }
+
+        [Test]
+        public void ReturnsSumSomeNumbers()
+        {
+            Assert.AreEqual(18, stringCalc.add("1,2,4,5,6"));
+        }
+
+        [Test]
+        public void ReturnsSumTwoNumbersNewLine()
+        {
+            Assert.AreEqual(3, stringCalc.add("1\n2"));
+        }
+
+        [Test]
+        public void ReturnsSumThreeNumbersMixedDelimiters()
+        {
+            Assert.AreEqual(6, stringCalc.add("1\n2,3"));
+        }
+
+        [Test]
+        public void ReturnSumDifferentDelimiters()
+        {
+            Assert.AreEqual(3, stringCalc.add("//;\n1;2"));
+        }
+    }
+}
